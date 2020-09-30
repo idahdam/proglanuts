6b63bf495a8f5223cfc6b8479b46018af672a1ee
idahdam <40880027+idahdam@users.noreply.github.com>
2020-09-30 13:12:09 +0700

    update 1

---
+++ README.md
@@ -0,0 +1,6 @@
+# proglanuts hadi jo
+gas
+Progress 30/9/2020
+
+1. Bikin 4x4 square array diisi dengan bilangan random 1-15
+2. Mulai dari kanan bawah, dan yang bakal digerakan itu 0 nya
---
+++ main.c
@@ -0,0 +1,16 @@
+
+// tracksaddas
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+
+#include "play.h"
+
+int main(int argc, char const *argv[]){
+    /* code */
+    startFunc();
+    return 0;
+}
