fca5bd51065425613129da97424821829f517d70
jonaa <56060812+jaflanet@users.noreply.github.com>
2020-10-10 01:21:40 +0700

    Update MainMenu.c

--- MainMenu.c
+++ MainMenu.c
@@ -1,8 +1,91 @@
 #include "selfHeader.h"
 
 void intro(){
+ int n=0;
+ int a=0;
+system("COLOR 8f");
+system("cls");
 
+for(n=0 ; n <= 13 ; n++){
+    a= n ;
+    if ( a == 1){
+    system("COLOR 8A");
+    printf("=====================================================\n");
+    Sleep(300);
     system("cls");
+    }
+    else if ( a == 2){
+        system("COLOR 8b");
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 3){
+        system("COLOR 8c");
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==      =                         =\n");
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 4){
+        system("COLOR 8d");
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==      =                         =\n");
+    printf("=                 ==      =======                   =\n");
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 5){
+        system("COLOR 8e");
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==      =                         =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==            =                   =\n");
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 6){
+        system("COLOR 8f");
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==      =                         =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==            =                   =\n");
+    printf("=                 ==      =======                   =\n");
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 7){
+        system("COLOR 8a");
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==      =                         =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==            =                   =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                                                   =\n");
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 8){
+        system("COLOR 8b");
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==      =                         =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==            =                   =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                                                   =\n");
+    printf("=                   PUZZLE GAME                     =\n");
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 9){
+        system("COLOR 8c");
     printf("=====================================================\n");
     printf("=                 ==      =======                   =\n");
     printf("=                 ==      =                         =\n");
@@ -12,9 +95,66 @@ void intro(){
     printf("=                                                   =\n");
     printf("=                   PUZZLE GAME                     =\n");
     printf("=====================================================\n");
-
-    printf("press any key to continue_");
-    getch();
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 10){
+        system("COLOR 8d");
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==      =                         =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==            =                   =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                                                   =\n");
+    printf("=                                                   =\n");
+    printf("=====================================================\n");
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 11){
+        system("COLOR 8e");
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==      =                         =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==            =                   =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                                                   =\n");
+    printf("=                   PUZZLE GAME                     =\n");
+    printf("=====================================================\n");
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 12){
+        system("COLOR 8f");
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==      =                         =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==            =                   =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                                                   =\n");
+    printf("=                                                   =\n");
+    printf("=====================================================\n");
+    Sleep(300);
+    system("cls");
+    }
+    else if ( a == 13){
+    printf("=====================================================\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==      =                         =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                 ==            =                   =\n");
+    printf("=                 ==      =======                   =\n");
+    printf("=                                                   =\n");
+    printf("=                   PUZZLE GAME                     =\n");
+    printf("=====================================================\n");
+    Sleep(700);
+    }
+}
+printf("press any key to continue...");
+getch();
 }
 
 void mainMenu(){
@@ -28,3 +168,4 @@ void mainMenu(){
     printf("=====================================================\n");
     printf("Input number to continue                             \n");
 }
+
